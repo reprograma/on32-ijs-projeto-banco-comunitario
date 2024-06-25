d07bdbbcf4c3861b8554208250c65e1925198393
Rafaela Atanasio <rah.jd@hotmail.com>
2024-06-24 22:28:49 -0300

    Criado lógica para sacar

--- src/classes/Conta.ts
+++ src/classes/Conta.ts
@@ -16,14 +16,23 @@ sacar(valor: number): Subtrai um valor do saldo se houver fundos suficientes.
 transferir(valor: number, contaDestino: NovaConta): Transfere um valor para outra conta.
 consultarSaldo(): Retorna o saldo atual da conta. */
 
-depositar(valor: number): void {
-    this.saldo += valor;
+depositar(valor: number): void { // adicionando valor no saldo
+    this.saldo += valor; // verificando se o saldo é maior ou igual ao valor solicitado (valor)
     console.log(`Depositado: R$${valor}. Saldo atual: R$${this.saldo}`);
 }
 
-sacar(valor: number): void {
-    if 
-}
+sacar(valor: number): void { // subtraindo um valor do saldo se houver fundos suficientes
+// verifica se o valor da propriedade saldo do objeto atual (this.saldo) é maior ou igual
+// ao valor solicitado (valor).
+    if (this.saldo >= valor){
+        this.saldo -= valor; // Ela subtrai o valor de valor do saldo atual (this.saldo).
+        // abreviado de this.saldo = this.saldo - valor
+        console.log(`Sacado: R$${valor}. Saldo atual: R$${this.saldo}`);
+    } else {
+            console.log(`Saldo insuficiente`)
+        }
+    }
+
 
 
 
